af9b1ffdf184f8467718015163eb8342ced960c8
Josh Leslie <hello@joshmleslie.com>
2023-05-23 19:09:40 -0700

    add teensy mac snippet

---
+++ teensy-get-mac/teensy-get-mac.ino
@@ -0,0 +1,14 @@
+byte mac[6];
+void teensyMAC(uint8_t *mac) {
+    Serial.println("Set MAC and refactor");
+    for(uint8_t by=0; by<2; by++) mac[by]=(HW_OCOTP_MAC1 >> ((1-by)*8)) & 0xFF;
+    for(uint8_t by=0; by<4; by++) mac[by+2]=(HW_OCOTP_MAC0 >> ((3-by)*8)) & 0xFF;
+    Serial.printf("MAC: %02x:%02x:%02x:%02x:%02x:%02x\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+}
+void setup() {
+  Serial.begin(1000000);
+  while(!Serial);
+  teensyMAC(mac);
+}
+
+void loop () {}
